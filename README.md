5dedf3463ac1c46d52da1c6b155de08e69c050ec
Yusuke Sakai <y.f.sakai@gmail.com>
2021-09-01 23:03:49 +0900

    Add sending message to Slack

--- README.md
+++ README.md
@@ -14,7 +14,10 @@ cp .env.sample .env
 
 Add the following environmental variables to `.env` file.
 
-- NOTION_TOKEN: A token for the Notion API.
+- `NOTION_TOKEN`: A token for the Notion API.
+- `NOTION_PAGE_ID`: ID of the page to process (uuid).
+- `SLACK_TOKEN`: A token for the Slack API.
+- `SLACK_CHANNEL`: Channel name or ID to send the message.
 
 ## Run!
 
--- app.ts
+++ app.ts
@@ -1,11 +1,30 @@
-import { Client } from "@notionhq/client";
+import { Client as NotionClient } from "@notionhq/client";
+import { WebClient as SlackClient } from "@slack/web-api";
 
 // Initialize a client
-const notion = new Client({
+const notion = new NotionClient({
   auth: process.env.NOTION_TOKEN,
 })
 
+// Post a message to a channel of Slack
+async function publishMessageToSlack(text: string) {
+  const slackWeb = new SlackClient(process.env.SLACK_TOKEN);
+  try {
+    await slackWeb.chat.postMessage({
+      channel: process.env.SLACK_CHANNEL,
+      text: text,
+    });
+    console.log("Message posted!");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 ;(async () => {
-  const listUsersResponse = await notion.users.list()
-  console.log(listUsersResponse)
+  const blocks = await notion.blocks.children.list({ block_id: process.env.NOTION_PAGE_ID });
+  for (const block of blocks.results) {
+    console.log(block[block.type])
+  }
+
+  // publishMessageToSlack("Test Message");
 })()
